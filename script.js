4130cf8f6bfaaee1ef2b517e9cc4f0ee1de73744
hardikk04 <hardik04062003@gmail.com>
2023-12-23 02:47:55 +0530

    page3 done

--- script.js
+++ script.js
@@ -19,8 +19,44 @@ t1.from(".page2>img", {
     scroller: "body",
     trigger: ".page2>img",
     start: "top 80%",
-    end: "top 0%",
+    end: "top -20%",
     // markers: true,
     scrub: 1,
   },
 });
+
+t1.from(".page3-box-elems", {
+  onComplete: counter,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".page3-box-elems",
+    start: "top 80%",
+    // markers: true,
+  },
+});
+
+let projectCounter = 130;
+let trainingCounter = 88;
+let awardCounter = 10;
+let indexCounter = 0;
+
+function counter() {
+  const myInterval = setInterval(() => {
+    indexCounter++;
+    if (indexCounter <= projectCounter) {
+      document.querySelector(".project").innerHTML = indexCounter;
+    }
+
+    if (indexCounter <= trainingCounter) {
+      document.querySelector(".training").innerHTML = indexCounter;
+    }
+
+    if (indexCounter <= awardCounter) {
+      document.querySelector(".awards").innerHTML = indexCounter;
+    }
+
+    if (indexCounter > 130) {
+      clearInterval(myInterval);
+    }
+  }, 50);
+}
